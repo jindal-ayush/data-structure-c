8db7e1642a33454b4b1288deb237493a4ae4eab0
PiyushJindal <piyush95ksp@gmail.com>
2022-11-13 00:27:55 +0530

    createNode function added

--- linked_list/max_min_element.c
+++ linked_list/max_min_element.c
@@ -1,68 +1,77 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-struct node{
+struct Node
+{
     int data;
-    struct node * next;
-    
-}*first = NULL;
+    struct node *next;
+
+} *head = NULL;
 
 void create(int A[], int n)
 {
-       int i;
-   struct node * last, *t ;
-   first = (struct node *)malloc(sizeof(struct node));
-   first->data = A[0];
-   first->next = NULL;
-   last = first;   
+    int i;
+    struct Node *previous, *t;
+    head = createNode(A[0]);
+    previous = head;
 
-   for(i= 1; i<n; i++){
-    t = (struct node*)malloc(sizeof(struct node));
-    t->data =A[i];
-    t->next = NULL;
-    last->next = t;
-    last = t;
-   }
+    for (i = 1; i < n; i++)
+    {
+        t = createNode(A[i]);
+        previous->next = t;
+        previous = t;
+    }
 }
-int max(struct node * p){
-   int max = INT_MIN;
-   while (p !=0)
-   {
-  
-      if(p->data>max)
-      max = p->data;
-      p = p->next;
-   };
-   return max;
+
+struct Node *createNode(int data)
+{
+    struct Node *node = (struct Node *)malloc(sizeof(struct Node *));
+    node->data = data;
+    node->next = NULL;
+    return node;
+}
+
+int maximum(struct Node *p)
+{
+    int max = INT_MIN;
+    while (p != 0)
+    {
+
+        if (p->data > max)
+            max = p->data;
+        p = p->next;
+    };
+    return max;
 };
-int min(struct node *p){
-   int min = INT_MAX;
-   while(p!=0)
-   {
-    if(p->data<min)
-       min  = p->data;
-         p = p->next;
-   }  
-   return min;
+int minimum(struct Node *p)
+{
+    int min = INT_MAX;
+    while (p != 0)
+    {
+        if (p->data < min)
+            min = p->data;
+        p = p->next;
+    }
+    return min;
 }
 
-int ReverseMax(struct node *p)
+int ReverseMax(struct Node *p)
 {
     int x = 0;
-    if(p==0)
-    return INT_MIN;
-    x= ReverseMax(p->next);
-    if(x>p->data)
+    if (p == 0)
+        return INT_MIN;
+    x = ReverseMax(p->next);
+    if (x > p->data)
         return x;
-    else return p->data;
+    else
+        return p->data;
 };
-int main(){
-    int A[5] = {11,2,13,4,5};
+int main()
+{
+    int A[5] = {11, 2, 13, 4, 5};
     create(A, 5);
-    printf("maximum element is %d\n",ReverseMax(first));
-   
-    printf("recusrive maximum element is %d\n",max(first));
-    printf("minimum element is %d ",min(first));
-}
+    printf("maximum element is %d\n", ReverseMax(head));
 
-    
+    printf("recusrive maximum element is %d\n", maximum(head));
+    printf("minimum element is %d ", minimum(head));
+}
